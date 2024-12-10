a5dd10dda7ec3cccd505a0f325e1947c53aeeff2
Derrick Hines <da.hines@cfpo.us>
2024-12-10 16:01:41 -0500

    Initial Commit

---
+++ GetHubExample/GetHubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetHubExampleApp.swift
+//  GetHubExample
+//
+//  Created by Derrick Hines on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GetHubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GetHubExampleTests/GetHubExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetHubExampleTests.swift
+//  GetHubExampleTests
+//
+//  Created by Derrick Hines on 12/10/24.
+//
+
+import Testing
+@testable import GetHubExample
+
+struct GetHubExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
